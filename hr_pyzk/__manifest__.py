2e91f0ca479eb89336e70bc0e2a542e29ef6a852
Sheikh M. Salahuddin <smsalah@gmail.com>
2019-02-02 19:02:45 +0500

    Update __manifest__.py

--- hr_pyzk/__manifest__.py
+++ hr_pyzk/__manifest__.py
@@ -19,14 +19,14 @@ It works on zkteco machines using pyzk library( version 1.9)
     'license': 'AGPL-3',
     'depends': ['hr', 'hr_attendance'],
     'data': [
-        'security/pyzk_security.xml',
-        'security/ir.model.access.csv',
         'views/device_users_view.xml',
         'views/devices_view.xml',
         'views/device_attendances_view.xml',
         'views/combined_attendances_view.xml',
         'wizard/user_wizard.xml',
-        'wizard/delete_attendance_wizard_view.xml'
+        'wizard/delete_attendance_wizard_view.xml',
+        'security/pyzk_security.xml',
+        'security/ir.model.access.csv',
      ],
     'installable': True,
     'auto_install': False,
